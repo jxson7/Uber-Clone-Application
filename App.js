63c94f3e0b641e1af4f4b47ee99f5cc8f5da8bbd
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-08-01 22:45:42 +0100

    transferring of files from previous repo

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
-import { store } from './store';
+import { store } from "./store";
 import HomeScreen from "./views/HomeScreen";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
