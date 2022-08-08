77f365e6a01921b0c056852f1c69390193d64bc4
JASON DOOKARUN <jason.dookarun@live.co.uk>
2022-08-08 17:44:48 +0100

    map illustration functional

--- views/MapScreen.js
+++ views/MapScreen.js
@@ -1,10 +1,22 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import tw from "tailwind-react-native-classnames";
+import Map from '../components/Map';
+import MapView from 'react-native-maps';
+
+
 
 const MapScreen = () => {
   return (
     <View>
       <Text>Here is the Map tings....</Text>
+      <View style={tw`h-1/2`}>
+
+      <Map />
+      </View>
+            
+      <View style={tw`h-1/2`}></View>      
+
     </View>
   )
 }
